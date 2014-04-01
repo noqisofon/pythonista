1e1381d1910499c1b26a56150878f1d5d3d09861
ned rihine <ned.rihine@gmail.com>
2014-04-02 06:10:24 +0900

    argument-parser-on とかぽりもーふぃずむの意味ない感じあるので parser-on に修正

--- examples/sumnum.scm
+++ examples/sumnum.scm
@@ -2,10 +2,15 @@
 (use pythonista.argparse)
 
 (let ((parser (make-argument-parser :description "Process some integers.")))
-  (argument-parser-on parser "integer" :metavar "N" :type <integer> :nargs "+"
-                      :help "an integer for the accumulator")
-  (argument-parser-on parser "--sum" :dest "accumulate" :action :store-const
-                      :const sum :default max
-                      :help "sum the integers (default: find the max)")
-  (let (args (argument-parser-parse-args parser))
-    (display (accumulate (ref integers args)))))
+  (parser-on parser "integer" :metavar "N" :type <integer> :nargs "+"
+             :help "an integer for the accumulator")
+  (parser-on parser "--sum" :dest :accumulate :action :store-const
+             :const (lambda (integers)
+                      (fold + 0 integers))
+             :default (lambda (integers)
+                        (fold max 0 integers))
+             :help "sum the integers (default: find the max)")
+  (let* ((args (parser-parse parser))
+         (integers (tree-map-get args :integer))
+         (accumulate (tree-map-get args :accumulate)))
+    (display (accumulate integers)))))
